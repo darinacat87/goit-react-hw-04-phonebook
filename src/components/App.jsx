e6808b01d1395eccf97490a8270368db5c8201ee
Daria Sliusar <darinacat87@gmail.com>
2022-11-16 00:36:23 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
@@ -6,26 +6,12 @@ import { Filter } from './Filter/Filter';
 import { SectionForm } from './SectionForm/SectionForm';
 
 export const App = () => {
-  const isFirstLoad = useRef(true);
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
-
+  const [contacts, setContacts] = useState(() => {
+    return JSON.parse(window.localStorage.getItem('contacts')) ?? [];
+  });
   const [filter, setFilter] = useState('');
 
   useEffect(() => {
-    if (isFirstLoad.current) {
-      let contacts = localStorage.getItem('contacts');
-      let parsedContacts = JSON.parse(contacts);
-      if (parsedContacts) {
-        setContacts(parsedContacts);
-      }
-      isFirstLoad.current = false;
-      return;
-    }
     localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
 
